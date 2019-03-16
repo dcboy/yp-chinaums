79020b9c60251045a674cf86680b32e8592e8322
DC <szdcboy@gmail.com>
2019-03-16 22:35:54 +0800

    fix: 测试用例使用mocha, createOrder不需要返回promise

--- src/chinaums.js
+++ src/chinaums.js
@@ -1,5 +1,5 @@
 const moment = require('moment');
-const querystring =require('querystring');
+const querystring = require('querystring');
 const axios = require("axios");
 import crypto from 'crypto';
 const APIURL = {
@@ -12,73 +12,75 @@ const APIURL = {
 class Chinaums {
   constructor(config) {
     this.config = config;
+
     // 基础参数
     this.baseOptions = {
       requestTimestamp: moment().format('YYYY-MM-DD HH:mm:ss'),
       mid: this.config.mid, // 商户号
     }
   }
-  md5Sign(data) {
+
+  md5Sign (data) {
     const md5 = crypto.createHash('md5').update(data).digest('hex');
     return md5;
   }
-  objKeySort(arys) {
-     // 先用Object内置类的keys方法获取要排序对象的属性名数组，再利用Array的sort方法进行排序
-     let newkey = Object.keys(arys).sort();
-     let newObj = ''; // 创建一个新的对象，用于存放排好序的键值对
-     for (let i = 0; i < newkey.length; i++) {
-         // 遍历newkey数组
-         if (arys[newkey[i]] === '') {
-            continue;
-         }
-         newObj += [newkey[i]] + '=' + arys[newkey[i]] + '&';
-     }
-     return newObj.substring(0, newObj.length - 1);
+
+  objKeySort (arys) {
+    // 先用Object内置类的keys方法获取要排序对象的属性名数组，再利用Array的sort方法进行排序
+    let newkey = Object.keys(arys).sort();
+    let newObj = ''; // 创建一个新的对象，用于存放排好序的键值对
+    for (let i = 0; i < newkey.length; i++) {
+      // 遍历newkey数组
+      if (arys[newkey[i]] === '') {
+        continue;
+      }
+      newObj += [newkey[i]] + '=' + arys[newkey[i]] + '&';
+    }
+    return newObj.substring(0, newObj.length - 1);
   }
- /**
-  * 发送请求到服务器
-  * @param {*} params 
-  * @param {*} options 
-  */
-  sendRequest(url, params, resolve, reject) {
+
+  /**
+   * 发送请求到服务器
+   * @param {*} params 
+   * @param {*} options 
+   */
+  sendRequest (url, params, resolve, reject) {
     try {
-      console.log(params)
-      axios.post(url, params).then( body => {
+      axios.post(url, params).then(body => {
         return resolve(body.data);
-    })
-    .catch(function (error) {
-      throw (new Error(error));
-    });
+      })
+        .catch(function (error) {
+          throw (new Error(error));
+        });
     } catch (ex) {
       return reject(new Error(`请求发生错误:${ex}`));
     }
   }
+
   /**
    * 3.6
    * 下单请求
    * @param {*} params.msgType
    */
-  createOrder(params) {
-    return new Promise((resolve, reject) => {
-      let options = {
-        msgType: 'WXPay.jsPay' , // 消息类型 WXPay.jsPay :微信公众号支付 trade.jsPay :支付宝 qmf.jspay :全民付 qmf.webPay :无卡 acp.jsPay :银联云闪付
-        instMid: 'YUEDANDEFAULT', // 业务类型
-      };
-      params = Object.assign({}, this.baseOptions, options, params);
-      const signstr = `${this.objKeySort(params)}${this.config.key}`
-      params.sign = this.md5Sign(signstr)
-      const link = `${APIURL.payUrl}?${querystring.stringify(params)}`;
-      console.log(link)
-      return link
-    })
+  createOrderUrl (params) {
+    let options = {
+      msgType: 'WXPay.jsPay', // 消息类型 WXPay.jsPay :微信公众号支付 trade.jsPay :支付宝 qmf.jspay :全民付 qmf.webPay :无卡 acp.jsPay :银联云闪付
+      instMid: 'YUEDANDEFAULT', // 业务类型
+    };
+    params = Object.assign({}, this.baseOptions, options, params);
+    const signstr = `${this.objKeySort(params)}${this.config.key}`
+    params.sign = this.md5Sign(signstr)
+    const link = `${APIURL.payUrl}?${querystring.stringify(params)}`;
+    return link
   }
+
   /**
    * 4.0
    * 支付结果查询
    * @param {*} params 配置信息
    * @return Promise<object> 查询的用户数据
    */
-  queryOrder(params) {
+  queryOrder (params) {
     return new Promise((resolve, reject) => {
       // 本接口的固定参数
       let options = {
@@ -89,8 +91,9 @@ class Chinaums {
       const signstr = `${this.objKeySort(params)}${this.config.key}`;
       params.sign = this.md5Sign(signstr);
       this.sendRequest(APIURL.commonUrl, params, resolve, reject)
-    })
+    });
   }
+
   /**
    * 7.3	银联订单退款请求
    * @param {*} params 
@@ -103,36 +106,18 @@ class Chinaums {
         instMid: 'YUEDANDEFAULT', // 业务类型
       };
       params = Object.assign({}, this.baseOptions, options, params);
-      console.log(this.objKeySort(params))
       const signstr = `${this.objKeySort(params)}${this.config.key}`;
       params.sign = this.md5Sign(signstr);
       this.sendRequest(APIURL.commonUrl, params, resolve, reject);
     });
   }
-  /**
-   * 8.1	银联订单退货请求
-   * @param {*} params 
-   * @return 退货回调结果
-   * 若多次退货，且后续退货上送的merOrderId和refundOrderId字段与之前退货上送的值一致，
-   * 将不会走退货逻辑，而是返回已有退货订单的退货信息
-   */
-  queryBackGoods(params) {
-    return new Promise((resolve, reject) => {
-      let options = {
-        msgType: 'refundQuery', // 消息类型
-      };
-      params = Object.assign({}, this.baseOptions, options, params);
-      const signstr = `${this.objKeySort(params)}${this.config.key}`;
-      params.sign = this.md5Sign(signstr);
-      this.sendRequest(APIURL.commonUrl, params, resolve, reject)
-    });
-  }
+
   /**
    * 9.1	关闭订单请求
    * @param {*} params 
    * @return 结果
    */
-  close(params) {
+  closeOrder (params) {
     return new Promise((resolve, reject) => {
       let options = {
         msgType: 'close', // 消息类型
@@ -144,12 +129,13 @@ class Chinaums {
       this.sendRequest(APIURL.commonUrl, params, resolve, reject)
     });
   }
+
   /**
    * * 11.1	支付结果通知
    * @param {*} params 
    * @return 结果
    */
-  getPayResult(params) {
+  getPayResult (params) {
     return new Promise((resolve, reject) => {
       let options = {
         instMid: 'YUEDANDEFAULT', // 消息类型
@@ -159,9 +145,10 @@ class Chinaums {
       params = Object.assign({}, this.baseOptions, options, params);
       const signstr = `${this.objKeySort(params)}${this.config.key}`;
       params.sign = this.md5Sign(signstr);
-      console.log(params)
       this.sendRequest(APIURL.commonUrl, params, resolve, reject)
     });
   }
 }
+
+//export
 module.exports = Chinaums;
--- test/index.js
+++ test/index.js
@@ -3,168 +3,23 @@ const Chinaums = require('../src/chinaums');
 const fs = require('fs');
 const path = require('path');
 const moment = require('moment');
-/**
-{ accessType: '0',
-  bizType: '000000',
-  currencyCode: '156',
-  encoding: 'UTF-8',
-  merId: '898440353313553',
-  orderId: '1547629517352',
-  queryId: '151901161705179895688',
-  respCode: '00',
-  respMsg: '成功[0000000]',
-  settleAmt: '1',
-  settleCurrencyCode: '156',
-  settleDate: '0116',
-  signMethod: '01',
-  signPubKeyCert: '-----BEGIN CERTIFICATE-----\r\nMIIEIDCCAwigAwIBAgIFEDRVM3AwDQYJKoZIhvcNAQEFBQAwITELMAkGA1UEBhMC\r\nQ04xEjAQBgNVBAoTCUNGQ0EgT0NBMTAeFw0xNTEwMjcwOTA2MjlaFw0yMDEwMjIw\r\nOTU4MjJaMIGWMQswCQYDVQQGEwJjbjESMBAGA1UEChMJQ0ZDQSBPQ0ExMRYwFAYD\r\nVQQLEw1Mb2NhbCBSQSBPQ0ExMRQwEgYDVQQLEwtFbnRlcnByaXNlczFFMEMGA1UE\r\nAww8MDQxQDgzMTAwMDAwMDAwODMwNDBA5Lit5Zu96ZO26IGU6IKh5Lu95pyJ6ZmQ\r\n5YWs5Y+4QDAwMDE2NDkzMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA\r\ntXclo3H4pB+Wi4wSd0DGwnyZWni7+22Tkk6lbXQErMNHPk84c8DnjT8CW8jIfv3z\r\nd5NBpvG3O3jQ/YHFlad39DdgUvqDd0WY8/C4Lf2xyo0+gQRZckMKEAId8Fl6/rPN\r\nHsbPRGNIZgE6AByvCRbriiFNFtuXzP4ogG7vilqBckGWfAYaJ5zJpaGlMBOW1Ti3\r\nMVjKg5x8t1/oFBkpFVsBnAeSGPJYrBn0irfnXDhOz7hcIWPbNDoq2bJ9VwbkKhJq\r\nVz7j7116pziUcLSFJasnWMnp8CrISj52cXzS/Y1kuaIMPP/1B0pcjVqMNJjowooD\r\nOxID3TZGfk5V7S++4FowVwIDAQABo4HoMIHlMB8GA1UdIwQYMBaAFNHb6YiC5d0a\r\nj0yqAIy+fPKrG/bZMEgGA1UdIARBMD8wPQYIYIEchu8qAQEwMTAvBggrBgEFBQcC\r\nARYjaHR0cDovL3d3dy5jZmNhLmNvbS5jbi91cy91cy0xNC5odG0wNwYDVR0fBDAw\r\nLjAsoCqgKIYmaHR0cDovL2NybC5jZmNhLmNvbS5jbi9SU0EvY3JsMjI3Mi5jcmww\r\nCwYDVR0PBAQDAgPoMB0GA1UdDgQWBBTEIzenf3VR6CZRS61ARrWMto0GODATBgNV\r\nHSUEDDAKBggrBgEFBQcDAjANBgkqhkiG9w0BAQUFAAOCAQEAHMgTi+4Y9g0yvsUA\r\np7MkdnPtWLS6XwL3IQuXoPInmBSbg2NP8jNhlq8tGL/WJXjycme/8BKu+Hht6lgN\r\nZhv9STnA59UFo9vxwSQy88bbyui5fKXVliZEiTUhjKM6SOod2Pnp5oWMVjLxujkk\r\nWKjSakPvV6N6H66xhJSCk+Ref59HuFZY4/LqyZysiMua4qyYfEfdKk5h27+z1MWy\r\nnadnxA5QexHHck9Y4ZyisbUubW7wTaaWFd+cZ3P/zmIUskE/dAG0/HEvmOR6CGlM\r\n55BFCVmJEufHtike3shu7lZGVm2adKNFFTqLoEFkfBO6Y/N6ViraBilcXjmWBJNE\r\nMFF/yA==\r\n-----END CERTIFICATE-----',
-  traceNo: '989568',
-  traceTime: '0116170557',
-  txnAmt: '1',
-  txnSubType: '01',
-  txnTime: '20190116170517',
-  txnType: '01',
-  version: '5.1.0',
-  signature: 'CJTXVcykQ6Jp8n1xW99V3OSLn4itycTWLHMaNtEVHRXy+iD3yLPTWRwCA23V5BJgAIuY7MuSZIWmi7b7/F8mG+8SwPLZFtaE5AZzDt4U6qh9HpYnoqSmB3me3XsqxfOnuX7UWMiCxTiydrfdk9Cso5Voims1vWuMysrlIHtpXKe5qXojzF8oJqp9zFNaLPOm9jk88W+CtxsQX+tVbu989pWDetVSrUDOM5T2agPlOzj7dz+3BisbVRImYroDboFSrm4D4wHQcaSZBWPOhv6e481SbZ5/kv+TF7U/o0xY/b0vU+1fWYKpOkOvtCu6ZPsEUK/CxIASFHG0GGy6J71f+w==' }
- */
-(async () => {
-  // let pfxBuffer = fs.readFileSync(path.resolve(`./config/898440353313553.pfx`));
-  // console.log(pfxBuffer.toString('base64'))
-  // const config = await UnionPay.processPfx(pfxBuffer, Config.password);
-  // config.merId = "898440353313553";
-  // let pfxBuffer = fs.readFileSync(path.resolve('/Users/mac/test.txt'),'utf-8');
-  // console.log(pfxBuffer)
-  const chinaums = new Chinaums({
-    mid: '898340149000005',
-    key:'fcAmtnx7MwismjWNhNKdHC44mNXtnEQeJkRrhKJwyrW2ysRR',
-  });
-  // const data = { 
-  //   accessType: '0',
-  //   bizType: '000000',
-  //   currencyCode: '156',
-  //   encoding: 'UTF-8',
-  //   merId: '898440353313553',
-  //   orderId: '1547644575226',
-  //   queryId: '251901162116153598228',
-  //   respCode: '00',
-  //   respMsg: '成功[0000000]',
-  //   settleAmt: '1',
-  //   settleCurrencyCode: '156',
-  //   settleDate: '0116',
-  //   signMethod: '01',
-  //   signPubKeyCert: '-----BEGIN CERTIFICATE-----\r\nMIIEIDCCAwigAwIBAgIFEDRVM3AwDQYJKoZIhvcNAQEFBQAwITELMAkGA1UEBhMC\r\nQ04xEjAQBgNVBAoTCUNGQ0EgT0NBMTAeFw0xNTEwMjcwOTA2MjlaFw0yMDEwMjIw\r\nOTU4MjJaMIGWMQswCQYDVQQGEwJjbjESMBAGA1UEChMJQ0ZDQSBPQ0ExMRYwFAYD\r\nVQQLEw1Mb2NhbCBSQSBPQ0ExMRQwEgYDVQQLEwtFbnRlcnByaXNlczFFMEMGA1UE\r\nAww8MDQxQDgzMTAwMDAwMDAwODMwNDBA5Lit5Zu96ZO26IGU6IKh5Lu95pyJ6ZmQ\r\n5YWs5Y+4QDAwMDE2NDkzMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA\r\ntXclo3H4pB+Wi4wSd0DGwnyZWni7+22Tkk6lbXQErMNHPk84c8DnjT8CW8jIfv3z\r\nd5NBpvG3O3jQ/YHFlad39DdgUvqDd0WY8/C4Lf2xyo0+gQRZckMKEAId8Fl6/rPN\r\nHsbPRGNIZgE6AByvCRbriiFNFtuXzP4ogG7vilqBckGWfAYaJ5zJpaGlMBOW1Ti3\r\nMVjKg5x8t1/oFBkpFVsBnAeSGPJYrBn0irfnXDhOz7hcIWPbNDoq2bJ9VwbkKhJq\r\nVz7j7116pziUcLSFJasnWMnp8CrISj52cXzS/Y1kuaIMPP/1B0pcjVqMNJjowooD\r\nOxID3TZGfk5V7S++4FowVwIDAQABo4HoMIHlMB8GA1UdIwQYMBaAFNHb6YiC5d0a\r\nj0yqAIy+fPKrG/bZMEgGA1UdIARBMD8wPQYIYIEchu8qAQEwMTAvBggrBgEFBQcC\r\nARYjaHR0cDovL3d3dy5jZmNhLmNvbS5jbi91cy91cy0xNC5odG0wNwYDVR0fBDAw\r\nLjAsoCqgKIYmaHR0cDovL2NybC5jZmNhLmNvbS5jbi9SU0EvY3JsMjI3Mi5jcmww\r\nCwYDVR0PBAQDAgPoMB0GA1UdDgQWBBTEIzenf3VR6CZRS61ARrWMto0GODATBgNV\r\nHSUEDDAKBggrBgEFBQcDAjANBgkqhkiG9w0BAQUFAAOCAQEAHMgTi+4Y9g0yvsUA\r\np7MkdnPtWLS6XwL3IQuXoPInmBSbg2NP8jNhlq8tGL/WJXjycme/8BKu+Hht6lgN\r\nZhv9STnA59UFo9vxwSQy88bbyui5fKXVliZEiTUhjKM6SOod2Pnp5oWMVjLxujkk\r\nWKjSakPvV6N6H66xhJSCk+Ref59HuFZY4/LqyZysiMua4qyYfEfdKk5h27+z1MWy\r\nnadnxA5QexHHck9Y4ZyisbUubW7wTaaWFd+cZ3P/zmIUskE/dAG0/HEvmOR6CGlM\r\n55BFCVmJEufHtike3shu7lZGVm2adKNFFTqLoEFkfBO6Y/N6ViraBilcXjmWBJNE\r\nMFF/yA==\r\n-----END CERTIFICATE-----',
-  //   traceNo: '359822',
-  //   traceTime: '0116211643',
-  //   txnAmt: '1',
-  //   txnSubType: '01',
-  //   txnTime: '20190116211615',
-  //   txnType: '01',
-  //   version: '5.1.0',
-  //   signature: 'EZ2rewkvaKzzTFwzo2BWeKBZS9LaI7VKdDwh95Gz7Ar/s135ysVJlpk103ejYwK2F21ACsNpn+nIUJ3HGpjKeu3UWA75zqV1sNhf6345SheVSObVRMwqQx+Q7jv6wkndZRhZSlmeIEWpoCNofPHRvCN41QW82Ksfyp8YPcuqybRfyMvxfr+hJVlybLpnMum5HhUvCMmZkNP6+FAEbNdr+QnjTwd+/hKGoSP0qBtkLb9LcjRMxVQxFmBOdxLnYfaLrFqVAqd57dFyUHDmSr6PYQPAP7u3Z1za9hK364fMiVXvOOUbsTZ8uWmB9Gb+rbfxEfW3GsFCyO5GpImtDyK8EQ=='
-  // }
+const confifg = require('../config/');
 
-  // const response = Chinaums.verifyTest(data);
-  // console.log(`验证信息---verify:${response}`);
-  /**
-   * 退款流程
-   * { requestTimestamp: '2019-03-16 04:15:25',
-      mid: '898340149000005',
-      msgType: 'refund',
-      instMid: 'YUEDANDEFAULT',
-      msgSrc: 'WWW.TEST.COM',
-      refundAmount: 1,
-      platformAmount: 0,
-      refundOrderId: '3194201806141508441197500',
-      totalAmount: 1,
-      merOrderId: '319420180614150753183991',
-      tid: '88880001',
-      sign: '767b8e6910aebdbf9bb368ee6f523704' }
-   * 返回参数结果...{"msgType":"wx.refund","payTime":"2019-03-16 04:15:26","connectSys":"OPENCHANNEL","msgSrc":"WWW.TEST.COM","merName":"仲晶晶二维码测试2","mid":"898340149000005","refundStatus":"SUCCESS","settleDate":"2019-03-16","settleRefId":"00466201244N","tid":"88880001","refundOrderId":"3194201806141508441197500","refundTargetOrderId":"50000710102019031608746652929","totalAmount":1,"targetMid":"265540495","responseTimestamp":"2019-03-16 04:15:29","errCode":"SUCCESS","targetStatus":"SUCCESS","seqId":"00466201245N","merOrderId":"319420180614150753183991","status":"TRADE_SUCCESS","targetSys":"WXPay","sign":"B0ED127DE3A60A881CF7672BEC683EFD"}
-   */
-  console.log('测试接口开始....')
-  const res = await chinaums.getPayResult({
-    msgSrc: 'WWW.TEST.COM',
-    billNo: '31940000201700002',
-    totalAmount: 1,
-    refundAmount: 1,
-    "goods": JSON.stringify([
-      {
-          "body": "微信二维码测试",
-          "price": "1",
-          "goodsName": "微信二维码测试",
-          "goodsId": "1",
-          "quantity": "1",
-          "goodsCategory": "TEST"
-      }
-  ]),
-  //   subOrders: JSON.stringify([
-  //     {
-  //         totalAmount: 1,
-  //         mid: "988460101800203"
-  //     }
-  //  ]),
-    // platformAmount: 0,
-    refundOrderId: "3194201806141508441197500",
-   // merOrderId: '319420180614150753183991',
-    totalAmount: '1',
-    // merOrderId: '319420180614150753183991',
-    tid: '88880001', // 终端号
+const chinaums = new Chinaums(confifg);
+const assert = require('assert');
+describe('订单相关', () => {
+  it('创建订单', async () => {
+    let res = await chinaums.createOrderUrl();
+    console.log(res);
+    assert.ok(res.indexOf('pay.do') > 0);
   });
-  console.log(`返回参数结果...${JSON.stringify(res)}`)
-  // 测试接口开始...
-  /**
-    * {
-    * 	accessType: '0',
-    * 	bizType: '000000',
-    * 	encoding: 'UTF-8',
-    * 	merId: '898440353313553',
-    * 	orderId: '1547632524375',
-    * 	origQryId: '151901161705179895688',
-    * 	queryId: '151901161755249910108',
-    * 	respCode: '00',
-    * 	respMsg: '成功[0000000]',
-    * 	signMethod: '01',
-    * 	txnAmt: '1',
-    * 	txnSubType: '00',
-    * 	txnTime: '20190116175524',
-    * 	txnType: '04',
-    * 	version: '5.1.0',
-    * 	signPubKeyCert: '-----BEGIN CERTIFICATE-----\r\nMIIEIDCCAwigAwIBAgIFEDRVM3AwDQYJKoZIhvcNAQEFBQAwITELMAkGA1UEBhMC\r\nQ04xEjAQBgNVBAoTCUNGQ0EgT0NBMTAeFw0xNTEwMjcwOTA2MjlaFw0yMDEwMjIw\r\nOTU4MjJaMIGWMQswCQYDVQQGEwJjbjESMBAGA1UEChMJQ0ZDQSBPQ0ExMRYwFAYD\r\nVQQLEw1Mb2NhbCBSQSBPQ0ExMRQwEgYDVQQLEwtFbnRlcnByaXNlczFFMEMGA1UE\r\nAww8MDQxQDgzMTAwMDAwMDAwODMwNDBA5Lit5Zu96ZO26IGU6IKh5Lu95pyJ6ZmQ\r\n5YWs5Y+4QDAwMDE2NDkzMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA\r\ntXclo3H4pB+Wi4wSd0DGwnyZWni7+22Tkk6lbXQErMNHPk84c8DnjT8CW8jIfv3z\r\nd5NBpvG3O3jQ/YHFlad39DdgUvqDd0WY8/C4Lf2xyo0+gQRZckMKEAId8Fl6/rPN\r\nHsbPRGNIZgE6AByvCRbriiFNFtuXzP4ogG7vilqBckGWfAYaJ5zJpaGlMBOW1Ti3\r\nMVjKg5x8t1/oFBkpFVsBnAeSGPJYrBn0irfnXDhOz7hcIWPbNDoq2bJ9VwbkKhJq\r\nVz7j7116pziUcLSFJasnWMnp8CrISj52cXzS/Y1kuaIMPP/1B0pcjVqMNJjowooD\r\nOxID3TZGfk5V7S++4FowVwIDAQABo4HoMIHlMB8GA1UdIwQYMBaAFNHb6YiC5d0a\r\nj0yqAIy+fPKrG/bZMEgGA1UdIARBMD8wPQYIYIEchu8qAQEwMTAvBggrBgEFBQcC\r\nARYjaHR0cDovL3d3dy5jZmNhLmNvbS5jbi91cy91cy0xNC5odG0wNwYDVR0fBDAw\r\nLjAsoCqgKIYmaHR0cDovL2NybC5jZmNhLmNvbS5jbi9SU0EvY3JsMjI3Mi5jcmww\r\nCwYDVR0PBAQDAgPoMB0GA1UdDgQWBBTEIzenf3VR6CZRS61ARrWMto0GODATBgNV\r\nHSUEDDAKBggrBgEFBQcDAjANBgkqhkiG9w0BAQUFAAOCAQEAHMgTi+4Y9g0yvsUA\r\np7MkdnPtWLS6XwL3IQuXoPInmBSbg2NP8jNhlq8tGL/WJXjycme/8BKu+Hht6lgN\r\nZhv9STnA59UFo9vxwSQy88bbyui5fKXVliZEiTUhjKM6SOod2Pnp5oWMVjLxujkk\r\nWKjSakPvV6N6H66xhJSCk+Ref59HuFZY4/LqyZysiMua4qyYfEfdKk5h27+z1MWy\r\nnadnxA5QexHHck9Y4ZyisbUubW7wTaaWFd+cZ3P/zmIUskE/dAG0/HEvmOR6CGlM\r\n55BFCVmJEufHtike3shu7lZGVm2adKNFFTqLoEFkfBO6Y/N6ViraBilcXjmWBJNE\r\nMFF/yA==\r\n-----END CERTIFICATE-----',
-    * 	signature: 'hJdB17OMvw2XfwyprXtsaTR5A35q/FdAzvqy0sATK+aZTmHdqTQDe/buz8gAlXf4FGibgr/2aYusRXSkaLHd7GTHpkFkLsqgjWWKSQDr+o/SvLjN8YrgEr4VYv2SOM9yKDjdTlIn+E0BpbVAX1VeD+aeYolDJXbSbCdM3KiPSyW07ofHfDZREzl2AA6zyY6Nc1uheS/GhpwA51cLd5QOsEL0/B+PqQPxpXjHIeGZTTMt9R5j7FRjdX2qH+F8XFML0auAHNdRcjTCUnH1GSoNPbyGzhPcnzBqkhChX1adyMc4HtdAfH4jOdAV4Oji9cvTFuWzg7R88ugF8VOobgLzvQ=='
-    * }
-  */
-  // const response = await unionPay.refunds({
-  //   txnAmt: 1,
-  //   orderId: (new Date()).getTime().toString(),
-  //   backUrl: 'http://6a3fdecd.ngrok.yopoint.cc:81/unionpay/gateway/notify/5c3eeaeccb2e21b9b64f35e0',
-  //   origQryId: '151901161705179895688',
-  // });
 
-  /** 退款回调
-{
-	"accessType": "0",
-	"bizType": "000000",
-	"currencyCode": "156",
-	"encoding": "UTF-8",
-	"merId": "898440353313553",
-	"orderId": "1547632524375",
-	"origQryId": "151901161705179895688",
-	"queryId": "151901161755249910108",
-	"respCode": "00",
-	"respMsg": "成功[0000000]",
-	"settleAmt": "1",
-	"settleCurrencyCode": "156",
-	"settleDate": "0116",
-	"signMethod": "01",
-	"signPubKeyCert": "-----BEGIN CERTIFICATE-----\r\nMIIEIDCCAwigAwIBAgIFEDRVM3AwDQYJKoZIhvcNAQEFBQAwITELMAkGA1UEBhMC\r\nQ04xEjAQBgNVBAoTCUNGQ0EgT0NBMTAeFw0xNTEwMjcwOTA2MjlaFw0yMDEwMjIw\r\nOTU4MjJaMIGWMQswCQYDVQQGEwJjbjESMBAGA1UEChMJQ0ZDQSBPQ0ExMRYwFAYD\r\nVQQLEw1Mb2NhbCBSQSBPQ0ExMRQwEgYDVQQLEwtFbnRlcnByaXNlczFFMEMGA1UE\r\nAww8MDQxQDgzMTAwMDAwMDAwODMwNDBA5Lit5Zu96ZO26IGU6IKh5Lu95pyJ6ZmQ\r\n5YWs5Y+4QDAwMDE2NDkzMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA\r\ntXclo3H4pB+Wi4wSd0DGwnyZWni7+22Tkk6lbXQErMNHPk84c8DnjT8CW8jIfv3z\r\nd5NBpvG3O3jQ/YHFlad39DdgUvqDd0WY8/C4Lf2xyo0+gQRZckMKEAId8Fl6/rPN\r\nHsbPRGNIZgE6AByvCRbriiFNFtuXzP4ogG7vilqBckGWfAYaJ5zJpaGlMBOW1Ti3\r\nMVjKg5x8t1/oFBkpFVsBnAeSGPJYrBn0irfnXDhOz7hcIWPbNDoq2bJ9VwbkKhJq\r\nVz7j7116pziUcLSFJasnWMnp8CrISj52cXzS/Y1kuaIMPP/1B0pcjVqMNJjowooD\r\nOxID3TZGfk5V7S++4FowVwIDAQABo4HoMIHlMB8GA1UdIwQYMBaAFNHb6YiC5d0a\r\nj0yqAIy+fPKrG/bZMEgGA1UdIARBMD8wPQYIYIEchu8qAQEwMTAvBggrBgEFBQcC\r\nARYjaHR0cDovL3d3dy5jZmNhLmNvbS5jbi91cy91cy0xNC5odG0wNwYDVR0fBDAw\r\nLjAsoCqgKIYmaHR0cDovL2NybC5jZmNhLmNvbS5jbi9SU0EvY3JsMjI3Mi5jcmww\r\nCwYDVR0PBAQDAgPoMB0GA1UdDgQWBBTEIzenf3VR6CZRS61ARrWMto0GODATBgNV\r\nHSUEDDAKBggrBgEFBQcDAjANBgkqhkiG9w0BAQUFAAOCAQEAHMgTi+4Y9g0yvsUA\r\np7MkdnPtWLS6XwL3IQuXoPInmBSbg2NP8jNhlq8tGL/WJXjycme/8BKu+Hht6lgN\r\nZhv9STnA59UFo9vxwSQy88bbyui5fKXVliZEiTUhjKM6SOod2Pnp5oWMVjLxujkk\r\nWKjSakPvV6N6H66xhJSCk+Ref59HuFZY4/LqyZysiMua4qyYfEfdKk5h27+z1MWy\r\nnadnxA5QexHHck9Y4ZyisbUubW7wTaaWFd+cZ3P/zmIUskE/dAG0/HEvmOR6CGlM\r\n55BFCVmJEufHtike3shu7lZGVm2adKNFFTqLoEFkfBO6Y/N6ViraBilcXjmWBJNE\r\nMFF/yA==\r\n-----END CERTIFICATE-----",
-	"traceNo": "464074",
-	"traceTime": "0116175524",
-	"txnAmt": "1",
-	"txnSubType": "00",
-	"txnTime": "20190116175524",
-	"txnType": "04",
-	"version": "5.1.0",
-	"signature": "dewRNymSV8xSidMrY2/q0wuQpvmkSZIWhD1C4HXQYPyg6bDzCAiAKFlacEY7IR9fw3mnNU+W5opO6SwZ59fsiOWovbwpiFhNQmPdNImA+5gk6swWNW2KsHdkcWvG5WMIYscTF7beC0Dk2AfmyrqNxCptfC2H1xWuId57KD50j6PbXkEpxP/O+P+rsbfIFjB8mcEospyeI6cBjGnN3n1WCxD/FhIu7mpx2rSt1E+nbCOMu4x7kFFGtc+CAJtIJLhfn3k8Y9MpTV3stS1bfotxocjwh6xt9jqS1VCbEigV8byI7SD1ffG7xQEyG69ix2hpthgOVKKateTujXvSoj/E0A=="
-}
-  */
-})();
+  it('查询订单', async () => {
+    // ...
+  });
+
+  it('订单退款', async () => {
+    // ...
+  });
+
+});
