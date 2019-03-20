f295778ac04137cc9e31505fddfee76d8092a02a
spar <493683987@qq.com>
2019-03-20 10:56:41 +0800

    style: 格式化代码更改telint报错处理;feat:增加了创建订单完成的验证处理

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-module.exports = require('./lib/chinaumspay');
+// module.exports = require('./lib/chinaums');
+const Chinaums = require('./src/chinaums');
+const Router = require('koa-router'); // x导入koa路由
+const Koa = require('koa');
+
+const app = new Koa();
+const router = new Router();
+const KoaBody = require('koa-bodyparser');
+// json
+app.use(KoaBody({}));
+const chinaums = new Chinaums({ key: 'fcAmtnx7MwismjWNhNKdHC44mNXtnEQeJkRrhKJwyrW2ysRR' });
+router.post('/', async (ctx) => {
+  const { body } = ctx.request;
+  if (body.status === 'TRADE_SUCCESS' && chinaums.verifyNotify(body)) {
+    ctx.body = 'SUCCESS';
+  } else {
+    ctx.body = 'FAILED';
+  }
+});
+app.use(router.routes());
+app.listen(3001, () => {
+  console.log('port 3001 is listening');
+});
--- test/index.js
+++ test/index.js
@@ -2,48 +2,52 @@ const Chinaums = require('../src/chinaums');
 const confifg = require('../config/');
 
 const moment = require('moment');
+
 const chinaums = new Chinaums(confifg);
 const assert = require('assert');
+
 // mocha测试用例
 describe('订单相关', () => {
-	it('创建订单', async () => {
-		let res =  chinaums.createOrderUrl({
-			tid: '88880001',
-			totalAmount: 1,
-			msgSrc: 'WWW.TEST.COM',
-			merOrderId: `3194${moment().format('YYYYMMDDHHmmss')}`,
-		});
-		console.log(res)
-		assert.ok(res.indexOf('pay.do'));
-	});
+  it('创建订单', async () => {
+    const res = chinaums.createOrderUrl({
+      tid: '88880001',
+      totalAmount: 1,
+      msgSrc: 'WWW.TEST.COM',
+      merOrderId: `3194${moment().format('YYYYMMDDHHmmss')}`,
+      notifyUrl: 'http://chinaums.frpgz1.idcfengye.com/',
+    });
+    console.log(res);
+    assert.ok(res.indexOf('pay.do'));
+  });
 
-	it('查询订单', async () => {
-		let res = await chinaums.queryOrder({
-      merOrderId: '319420190318104733',
+  it('查询订单', async () => {
+    const res = await chinaums.queryOrder({
+      merOrderId: '319420190319152115',
       tid: '88880001',
       msgSrc: 'WWW.TEST.COM',
-		});
-		console.log(res)
-		assert.ok(res);
-	});
+    });
+    console.log(res);
+    assert.ok(res);
+  });
 
-	it('订单退款', async () => {
-		let res = await chinaums.refunds({
-      merOrderId: '319420190318104733',
-      tid: '88880001',
-      msgSrc: 'WWW.TEST.COM',
-		});
-		console.log(res)
-		assert.ok(res);
-	});
+  // it('订单退款', async () => {
+  // 	let res = await chinaums.refunds({
+  // 		merOrderId: '319420190319135454',
+  // 		tid: '88880001',
+  // 		refundAmount: 1,
+  // 		msgSrc: 'WWW.TEST.COM'
+  // 	});
+  // 	console.log(res);
+  // 	assert.ok(res);
+  // });
 
-	it('关闭订单', async () => {
-		let res = await chinaums.closeOrder({
-      msgSrc: 'WWW.TEST.COM',
-      merOrderId: '319420190318105008',
-      tid: '88880001',
-		});
-		console.log(res)
-		assert.ok(res);
-	});
+  // it('关闭订单', async () => {
+  // 	let res = await chinaums.closeOrder({
+  // 		msgSrc: 'WWW.TEST.COM',
+  // 		merOrderId: '319420190318105008',
+  // 		tid: '88880001',
+  // 	});
+  // 	console.log(res);
+  // 	assert.ok(res);
+  // });
 });
